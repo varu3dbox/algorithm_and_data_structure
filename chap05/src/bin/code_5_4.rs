f2f92701d82483645283110b5e7711a90f3d59f6
varu3dbox <subplus.1027magi+tech@gmail.com>
2021-01-21 02:01:53 +0900

    code_5_4 and code_5_5

--- chap05/src/bin/code_5_4.rs
+++ chap05/src/bin/code_5_4.rs
@@ -20,12 +20,15 @@ fn main() {
     let mut dp = vec![100000; N];
     dp[0] = 0;
 
-    for i in 1..N {
-        let tmp = dp[i - 1] + (h[i] - h[i - 1]).abs();
-        chmin(&mut dp[i], tmp);
-        if i > 1 {
-            let tmp = dp[i - 2] + (h[i] - h[i - 2]).abs();
-            chmin(&mut dp[i], tmp);
+    for i in 0..N {
+        if i + 1 < N {
+            let tmp = dp[i] + (h[i] - h[i + 1]).abs();
+            chmin(&mut dp[i + 1], tmp);
+        }
+
+        if i + 2 < N {
+            let tmp = dp[i] + (h[i] - h[i + 2]).abs();
+            chmin(&mut dp[i + 2], tmp);
         }
     }
 
