484e44a4d0dd8828ddab6d2b3e3f439101ed2bf5
varu3dbox <subplus.1027magi+tech@gmail.com>
2021-02-03 02:22:00 +0900

    ALDS1_1_A.rs

---
+++ procon_book/src/bin/ALDS1_1_A.rs
@@ -0,0 +1,23 @@
+use proconio::input;
+use std::cmp;
+
+fn main() {
+    //    input! {
+    //        n: usize,
+    //        R: [i32; n],
+    //    }
+    let n: usize = 6;
+    let mut source: Vec<i32> = vec![5, 2, 4, 6, 1, 3];
+
+    for i in 1..source.len() {
+        let mut j: usize = i;
+
+        while j > 0 && source[j - 1] > source[j] {
+            source.swap(j - 1, j);
+            println!("{:?}", source);
+            j -= 1;
+        }
+
+        println!("{:?}", source);
+    }
+}
